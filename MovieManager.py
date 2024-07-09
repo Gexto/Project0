bb789146f9ae01cda5791064b70c5b3b40423595
Gerardo Torres <gerardo.torres@saintleo.edu>
2024-07-09 10:12:55 -0400

    Added 3 functions under MovieManager.

--- MovieManager.py
+++ MovieManager.py
@@ -4,3 +4,17 @@ import json
 class MovieManager:
     def __init__(self):
         self.movies = []
+        
+    def add_movie(self, movie):
+        self.movies.append(movie)
+
+    def find_movie(self, title):
+        for movie in self.movies:
+            if movie.title.lower() == title.lower():
+                return movie
+        return None
+
+    def remove_movie(self, title):
+        movie = self.find_movie(title)
+        if movie:
+            self.movies.remove(movie)
