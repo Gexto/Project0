da8eee1b350380e233bed2df033d048a5476f354
Gerardo Torres <gerardo.torres@saintleo.edu>
2024-07-09 11:39:18 -0400

    Added print statements. And imported other files.

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("Hello world")
+from movie import movie 
+from MovieManager import MovieManager
+
+def main():
+     manager = MovieManager()
+
+    while True:
+        print("1. Add Movie")
+        print("2. Search Movie")
+        print("3. Update Movie")
+        print("4. Delete Movie")
+        print("5. List All Movies")
+        print("6. Mark as Watched/Unwatched")
+        print("7. Generate Reports")
+        print("8. Save to File")
+        print("9. Load from File")
+        print("10. Exit")
+    
+if __name__ == "__main__":
+    main()   
