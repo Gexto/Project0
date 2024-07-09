3497eacd4358a2bea5d76fb12acb1ca529a1a04b
Gerardo Torres <gerardo.torres@saintleo.edu>
2024-07-09 10:50:10 -0400

    Added list_movies & sort_movies functions in MovieManager.

--- MovieManager.py
+++ MovieManager.py
@@ -4,10 +4,13 @@ import json
 class MovieManager:
     def __init__(self):
         self.movies = []
-        
+
     def add_movie(self, movie):
         self.movies.append(movie)
 
+    #This function compares the title of the current movie 
+    # (converted to lowercase) with the input title (also converted to lowercase). 
+    #ensures that the comparison is case-insensitive
     def find_movie(self, title):
         for movie in self.movies:
             if movie.title.lower() == title.lower():
@@ -18,3 +21,12 @@ class MovieManager:
         movie = self.find_movie(title)
         if movie:
             self.movies.remove(movie)
+    
+    def list_movies(self):
+        for movie in self.movies:
+            print(movie)
+
+    def sort_movies(self, key):
+        #sort movies based on the specified attribute.
+        #the lambda function extracts the attribute value for sorting.
+        self.movies.sort(key=lambda x: getattr(x, key))
